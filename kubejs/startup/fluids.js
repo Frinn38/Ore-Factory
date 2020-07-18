730a37c394c1a634adc76c7e3ce1c0e7bfdf1d61
Emalios <35004924+Emalios@users.noreply.github.com>
2020-07-18 21:55:48 +0200

    Add ores solution and craft for them.

--- kubejs/startup/fluids.js
+++ kubejs/startup/fluids.js
@@ -1,4 +1,10 @@
 events.listen('fluid.registry', function (event) {
   event.create("purified_water").color(-97929511).luminosity(0);
   event.create("sludge").color(8210436).luminosity(0);
+  event.create("bananium_solution").color(14800947).luminosity(0);
+  event.create("olivium_solution").color(5421606).luminosity(0);
+  event.create("purpurite_solution").color(10298321).luminosity(0);
+  event.create("ravenite_solution").color(2236962).luminosity(0);
+  event.create("rubium_solution").color(13572120).luminosity(0);
+  event.create("saphirium_solution").color(1653241).luminosity(0);
 })
